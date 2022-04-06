c2d56d959e8193e921b34bdb043058cfa388d78f
mtrzyna <mateusz.trzyna@nexio.pl>
2022-04-07 00:23:21 +0200

    fix jump and add some animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,13 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D playerRB;
     public float moveSpeed;
     public float jumpForce;
-    public float velocity;
+    private float velocity;
+
+    private bool isGrounded;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+
+    public Animator playerAnimator;
 
     void Start()
     {
@@ -17,15 +23,37 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, 0.2f, whatIsGround);
         playerRB.velocity = new Vector2(velocity * moveSpeed, playerRB.velocity.y);
-       
+
+        Animate();
+    }
+
+    private void Animate()
+    {
+        playerAnimator.SetBool("isGrounded", isGrounded);
+        playerAnimator.SetFloat("speed", Mathf.Abs(playerRB.velocity.x));
+        playerAnimator.SetFloat("ySpeed", playerRB.velocity.y);
+        if (playerRB.velocity.x < 0)
+        {
+            playerRB.transform.localScale = new Vector3(-1f, 1f, 1f);
+        } 
+        else if (playerRB.velocity.x > 0)
+        {
+            playerRB.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.started && isGrounded)
         {
             playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+        }
+
+        if(!isGrounded && context.canceled && playerRB.velocity.y > 0)
+        {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, playerRB.velocity.y * 0.5f);
         }
     }
 
