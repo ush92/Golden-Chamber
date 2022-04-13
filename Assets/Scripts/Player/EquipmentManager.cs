ee135418846a96314082e39dc472c741284a4c51
mtrzyna <mateusz.trzyna@nexio.pl>
2022-04-13 02:18:05 +0200

    start to do eq manager

---
+++ Assets/Scripts/Player/EquipmentManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour
+{
+    public List<int> weapons;
+    public List<int> utlities;
+    public List<int> collectables;
+    public List<int> questItems;
+
+    public int currentWeapon;
+    public int currentUtility; 
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
