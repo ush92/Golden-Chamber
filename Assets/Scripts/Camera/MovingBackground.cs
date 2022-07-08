199df8f7d453332cd9dcbc50dc11049694fc278e
mtrzyna <mateusz.trzyna@nexio.pl>
2022-07-08 19:27:50 +0200

    fix main menu camera

--- Assets/Scripts/Camera/MovingBackground.cs
+++ Assets/Scripts/Camera/MovingBackground.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class MovingBackground : MonoBehaviour
 {
     private int index = 0;
+    private float speed = 0.1f;
 
     void FixedUpdate()
     {
@@ -11,19 +12,19 @@ public class MovingBackground : MonoBehaviour
 
         if(index >= 0 && index < 200)
         {
-            transform.position = new Vector3(transform.position.x + 1f, transform.position.y + 1, transform.position.z);
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y + speed, transform.position.z);
         }
         else if (index >= 200 && index < 400)
         {
-            transform.position = new Vector3(transform.position.x - 1f, transform.position.y + 1, transform.position.z);
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y + speed, transform.position.z);
         }
         else if (index >= 400 && index < 600)
         {
-            transform.position = new Vector3(transform.position.x - 1f, transform.position.y - 1, transform.position.z);
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y - speed, transform.position.z);
         }
         else if (index >= 600 && index < 800)
         {
-            transform.position = new Vector3(transform.position.x + 1f, transform.position.y - 1, transform.position.z);
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y - speed, transform.position.z);
         }
         else
         {
