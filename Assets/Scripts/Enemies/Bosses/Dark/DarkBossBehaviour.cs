785131d9e3eee112cd28d6b35310ab35539b2a3d
mtrzyna <mateusz.trzyna@nexio.pl>
2022-08-11 03:27:39 +0200

    finish dark boss main behaviour

--- Assets/Scripts/Enemies/Bosses/Dark/DarkBossBehaviour.cs
+++ Assets/Scripts/Enemies/Bosses/Dark/DarkBossBehaviour.cs
@@ -8,6 +8,7 @@ public class DarkBossBehaviour : MonoBehaviour
     public Animator animator;
     public Transform basePosition;
     public Transform bottomPosition;
+    public List<Transform> upperPositions;
     private bool isUpper;
 
     public float teleportRepeatingTime;
@@ -19,11 +20,13 @@ public class DarkBossBehaviour : MonoBehaviour
     public EnemyPatrol spider;
     public float spidersNormalMoveSpeed;
     public float spidersEnragedMoveSpeed;
+    public List<SpriteRenderer> spidersCounterIndicators;
 
     public GameObject portals;
-    public float portalRemoveTime;
     public GameObject portalsRemoveEffect;
 
+    public float enrageTime;
+    public int spidersEnragedCount;
     private bool isEnraged;
     public EnemyBasicShoot shooting;
     public float enragedSpawnRepeatingTime;
@@ -36,7 +39,7 @@ public class DarkBossBehaviour : MonoBehaviour
         spider.moveSpeed = spidersNormalMoveSpeed;
         InvokeRepeating("Teleport", startDelay, teleportRepeatingTime);
         InvokeRepeating("SpawnSpiders", startDelay, spawnRepeatingTime);
-        Invoke("RemovePortals", portalRemoveTime);
+        Invoke("RemovePortals", enrageTime);
     }
 
     private void OnEnable()
@@ -44,9 +47,40 @@ public class DarkBossBehaviour : MonoBehaviour
         
     }
 
+    private void FixedUpdate()
+    {
+        spidersList.RemoveAll(item => item == null);
+
+        if (!isEnraged)
+        {
+            for (int i = 0; i < spidersCounterIndicators.Count; i++)
+            {
+                if (spidersList.Count > i)
+                {
+                    spidersCounterIndicators[i].color = new Color32(255, 0, 255, 200);
+                }
+                else
+                {
+                    spidersCounterIndicators[i].color = new Color32(0, 0, 0, 200);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < spidersCounterIndicators.Count; i++)
+            {
+                spidersCounterIndicators[i].color = new Color32(255, 0, 0, 200);       
+            }
+        }
+    }
+
     void Update()
     {
-        
+        if(spidersList.Count > spidersEnragedCount && !isEnraged)
+        {
+            isEnraged = true;
+            Invoke("RemovePortals", 0);
+        }
     }
 
     void Teleport()
@@ -60,7 +94,7 @@ public class DarkBossBehaviour : MonoBehaviour
         }
         else
         {
-            transform.position = basePosition.transform.position;
+            transform.position = upperPositions[Random.Range(0,3)].transform.position;
         }
 
         Instantiate(teleportEffect, transform.position, transform.rotation);
@@ -79,11 +113,23 @@ public class DarkBossBehaviour : MonoBehaviour
 
     private void RemovePortals()
     {
-        Instantiate(portalsRemoveEffect, new Vector3(portals.transform.position.x, portals.transform.position.y - 0.5f),
-            portals.transform.rotation);
+        isEnraged = true;
+
+        if (isUpper)
+        {
+            Instantiate(teleportEffect, transform.position, transform.rotation);
+            animator.SetTrigger("teleport");
+            transform.position = bottomPosition.transform.position;
+        }
+
+        Instantiate(teleportEffect, transform.position, transform.rotation);
+
+        isUpper = !isUpper;
+
+
+        Instantiate(portalsRemoveEffect, new Vector3(portals.transform.position.x, portals.transform.position.y - 0.5f), portals.transform.rotation);
         portals.SetActive(false);
 
-        isEnraged = true;
         spider.moveSpeed = spidersEnragedMoveSpeed;
         foreach(var spawnedSpider in spidersList)
         {
@@ -97,9 +143,19 @@ public class DarkBossBehaviour : MonoBehaviour
 
     private void OnDestroy()
     {
+        portals.SetActive(false);
+
         foreach (var spawnedSpider in spidersList)
         {
-            Destroy(spawnedSpider.gameObject);
+            if (spawnedSpider != null)
+            {
+                Destroy(spawnedSpider.gameObject);
+            }
+        }
+
+        for (int i = 0; i < spidersCounterIndicators.Count; i++)
+        {
+            spidersCounterIndicators[i].color = new Color32(0, 0, 0, 200);
         }
     }
 }
