52a7bad381d85e6a6ac0f655613a0c42404f319d
mtrzyna <mateusz.trzyna@nexio.pl>
2022-08-12 04:35:34 +0200

    balance poison boss

--- Assets/Scripts/Enemies/Bosses/Poison/PoisonBehaviour.cs
+++ Assets/Scripts/Enemies/Bosses/Poison/PoisonBehaviour.cs
@@ -4,6 +4,8 @@ public class PoisonBehaviour : MonoBehaviour
 {
     public PlayerController player;
     public GameObject toxinDebuf;
+    public Animator animator;
+    public GameObject debuffParticles;
 
     void Start()
     {
@@ -14,6 +16,8 @@ public class PoisonBehaviour : MonoBehaviour
     {
         if (player != null)
         {
+            animator.SetTrigger("Shoot");
+            Instantiate(debuffParticles, transform.position, transform.rotation);
             GameObject toxin = Instantiate(toxinDebuf);
             toxin.transform.parent = player.transform;
             toxin.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 0.6f, player.transform.position.z);
